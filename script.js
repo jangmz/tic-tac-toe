502f9fc6e8794b5fd9a60eba86c2f1f751613ac4
jangmz <131482887+jangmz@users.noreply.github.com>
2023-11-06 15:13:44 +0100

    created 2d array, method for getting board for the UI render, setMarker function

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-(function() {
-    const Gameboard = {
-        gameboard: new Array(9)
-    }
-    const playerX = {
-        marker: "x"
-    }
-    const playerO = {
-        marker: "o"
+function Gameboard() {
+    const row = 3;
+    const col = 3;
+    const board = [];
+    
+    // creating a 2D array gameboard
+    for (let i = 0; i < row; i++) {
+        board[i] = [];
+        for (let j = 0; j < col; j++){
+            board[i][j] = "?"; // change here with the player mark
+        }
     }
 
-    function renderGame() {
-        
+    // method for getting the entire board that UI will render
+    const getBoard = () => board;
+
+    // set a marker (player === marker)
+    const setMarker = (row, column, player) => {
+        if (board[row][column] === "?") {
+            board[row][column] = player;
+        }
     }
-})()
+
+    return {getBoard};
+}
+
+function Players() {
+
+}
+
+function Game() {
+
+}
+
+Gameboard();
