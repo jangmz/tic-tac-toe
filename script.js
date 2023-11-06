00b16ed33b417f6788a7897ba0604ca3bff633b9
jangmz <131482887+jangmz@users.noreply.github.com>
2023-11-06 11:33:32 +0100

    base for the objects

---
+++ script.js
@@ -0,0 +1,15 @@
+(function() {
+    const Gameboard = {
+        gameboard: new Array(9)
+    }
+    const playerX = {
+        marker: "x"
+    }
+    const playerO = {
+        marker: "o"
+    }
+
+    function renderGame() {
+        
+    }
+})()
